055b124cb4fd5e9b520835a7a4c0ddb07024048d
张顶炜 <zhangdingwei@souche.com>
2016-02-19 17:25:44 +0800

    fix

--- js/app.js
+++ js/app.js
@@ -18,7 +18,13 @@ deerResume.config(['$routeProvider',
       otherwise({
         redirectTo: '/resume'
       });
-  }]);
+}]);
+
+deerResume.config(['$httpProvider', function($httpProvider) {
+  $httpProvider.defaults.useXDomain = true;
+  $httpProvider.defaults.headers.common = 'Content-Type: application/json';
+  delete $httpProvider.defaults.headers.common['X-Requested-With'];
+}]);
 
 
 deerResume.controller('resumeCtrl', function ($scope,$http,storage) {
